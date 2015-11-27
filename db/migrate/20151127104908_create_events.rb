7ca6cf7b668ef7a89e585126701c7560ef9e50b7
v-kabashnikov <vania.kabashnikov@gmail.com>
2015-11-27 13:57:24 +0300

    added migrations

---
+++ db/migrate/20151127104908_create_events.rb
@@ -0,0 +1,12 @@
+class CreateEvents < ActiveRecord::Migration
+  def change
+    create_table :events do |t|
+      t.string :content
+      t.string :link
+      t.integer :user_id, default: false, null: false
+      t.string :event_type, default: false, null: false
+
+      t.timestamps null: false
+    end
+  end
+end
