c53e50a56e011b1de77a65af6fd9ed2b4106fbb0
Kabashnikov <vania.kabashnikov@gmail.com>
2015-11-27 14:02:15 +0300

    Update notes_controller.rb

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,45 @@
-class Notes_controller
+class NotesController < ApplicationController
+  before_action :set_note, only: [:show, :edit, :update, :destroy]
+
+  # POST /notes
+  def create
+    @order = Order.find(params[:current_order])
+
+    if @order.note != params[:note]
+      @order.note = params[:note]
+      @order.save
+    end
+    if @order.commentary != params[:commentary]
+      @order.commentary = params[:commentary]
+      @order.save
+    end
+
+    redirect_to :back
+  end
+
+  # PATCH/PUT /notes/1
+  def update
+    if @note.update(note_params)
+      redirect_to @note, notice: 'Note was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /notes/1
+  def destroy
+    @note.destroy
+    redirect_to notes_url, notice: 'Note was successfully destroyed.'
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_note
+    @note = Note.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def note_params
+    params[:note]
+  end
 end
