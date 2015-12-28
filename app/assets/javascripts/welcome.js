bb89a5068df8209fe9c4aed97a91eb6aa4006f69
v-kabashnikov <vania.kabashnikov@gmail.com>
2015-12-28 13:49:34 +0300

    added forms for uploading files

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -7,3 +7,21 @@
 //= require_tree .
 //= require maskedinput
 //= require bootstrap-datetimepicker
+
+$(document).ready(function(){
+    var counter = 2;
+    $('#del_file').hide();
+    $('img#add_file').click(function(){
+        $('#file_tools').before('<div class="file_upload oneItem"><input type="text" class="form-control" placeholder="Прикрепить файлы" id="f'+counter+'" /><div class="imgPort"><img class="text-right img-resonsive" src="/assets/download-6c71f202bfc944e0be652d95a45787163d9f8f54ea79088dbb743bbfe868f37b.png" alt="Download 6c71f202bfc944e0be652d95a45787163d9f8f54ea79088dbb743bbfe868f37b"></div><input type="file" name="materials[]" id="materials_" onchange="document.getElementById(\'f'+counter+'\').value = this.value;" /></div>');
+        $('#del_file').fadeIn(0);
+    counter++;
+    });
+    $('img#del_file').click(function(){
+        if(counter==3){
+            $('#del_file').hide();
+        }   
+        counter--;
+        $('#f'+counter).remove();
+    });
+});
+
