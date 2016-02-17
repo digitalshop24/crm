7614fef69688dd0cced2a4ccd37a14530e48bcab
Ivan Kabashnikov <vania.kabashnikov@gmail.com>
2016-02-17 18:04:43 +0300

    asdasd

--- db/migrate/20160217095415_create_seos.rb
+++ db/migrate/20160217095415_create_seos.rb
@@ -1,5 +1,13 @@
 class CreateSeos < ActiveRecord::Migration
   def change
+    create_table :seos do |t|
+      t.string :name
+      t.string :description
+      t.string :title
+      t.timestamps null: false
+  end
+  end
+  def change
     add_column :seos, :code_title, :string
   end
 end
