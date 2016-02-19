544772244f8015dd26767612cd297249435b6259
Ivan Kabashnikov <vania.kabashnikov@gmail.com>
2016-02-19 15:31:20 +0300

    redactor

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -11,7 +11,8 @@
 //= require plugins/align.min.js
 //= require plugins/char_counter.min.js
 //= require plugins/code_beautifier.min.js
-//= require plugins/code_view.min.js
+//= require plugins/image_manager.min.js
+//= require plugins/image.min.js
 //= require plugins/colors.min.js
 //= require plugins/font_family.min.js
 //= require plugins/font_size.min.js
