f2499e7eae5b33bd164123d56a3c35723231b5ef
Ivan Kabashnikov <vania.kabashnikov@gmail.com>
2016-02-19 14:26:16 +0300

    routes magic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   before_filter :configure_permitted_parameters, if: :devise_controller?
   before_filter :set_current_user
   before_filter :init
+  before_filter :add_allow_credentials_headers
   layout :layout_by_resource
 
   rescue_from CanCan::AccessDenied do |exception|
@@ -30,6 +31,18 @@ class ApplicationController < ActionController::Base
 
   def after_sending_reset_password_instructions_path_for(resource_name)
     edit_user_registration_path
+  end
+  def add_allow_credentials_headers                                                                                                                                                                                                                                                        
+    # https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS#section_5                                                                                                                                                                                                      
+    #                                                                                                                                                                                                                                                                                       
+    # Because we want our front-end to send cookies to allow the API to be authenticated                                                                                                                                                                                                   
+    # (using 'withCredentials' in the XMLHttpRequest), we need to add some headers so                                                                                                                                                                                                      
+    # the browser will not reject the response                                                                                                                                                                                                                                             
+    response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] || '*'                                                                                                                                                                                                     
+    response.headers['Access-Control-Allow-Credentials'] = 'true'                                                                                                                                                                                                                          
+  end 
+  def options                                                                                                                                                                                                                                                                              
+    head :status => 200, :'Access-Control-Allow-Headers' => 'accept, content-type'                                                                                                                                                                                                         
   end
 
   protected
