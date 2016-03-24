5e9d936c7c6d21c66d805f48bf39b6de6d27f698
Ivan Kabashnikov <vania.kabashnikov@gmail.com>
2016-03-24 22:37:54 +0300

    sdf

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,9 @@ module Crm
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+    config.assets.precompile += Ckeditor.assets
+    config.assets.precompile += %w(ckeditor/*)
     config.i18n.load_path += Dir[Rails.root.join("config/locales/**/*.{rb,yml}").to_s]
     config.i18n.default_locale = :ru
 
