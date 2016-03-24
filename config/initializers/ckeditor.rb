151a2a92ca3b7ac65250b98ec7767c7556aab3b1
Ivan Kabashnikov <vania.kabashnikov@gmail.com>
2016-03-24 22:46:04 +0300

    sdf

--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -21,7 +21,9 @@ Ckeditor.setup do |config|
   # config.authorize_with :cancan
 
   # Asset model classes
-  # config.picture_model { Ckeditor::Picture }
+  config.picture_model { Ckeditor::Epicture }
+
+
   # config.attachment_file_model { Ckeditor::AttachmentFile }
 
   # Paginate assets
