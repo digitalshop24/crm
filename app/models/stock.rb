17ead64d8d9385483a1ba383990862b73236c17e
Ivan Kabashnikov <vania.kabashnikov@gmail.com>
2016-04-06 18:03:37 +0300

    df

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,5 @@
 class Stock < ActiveRecord::Base
- has_attached_file :image
+ has_attached_file :image,
+    styles: { thumb: "200x200", medium: "500x500>", big: "1000x1000>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
